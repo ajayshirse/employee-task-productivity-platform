cfad00d4649d679db66f88b9ca51a194951907b4
ajayshirse <ajayshirse9665@gmail.com>
2026-08-20 00:39:28 +0530

    Initial project setup

---
+++ src/main/java/com/ajay/employee_task_productivity_platform/EmployeeTaskProductivityPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.ajay.employee_task_productivity_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeTaskProductivityPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeTaskProductivityPlatformApplication.class, args);
+	}
+
+}
